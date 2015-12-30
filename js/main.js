00cd5a77d6f17f18614376a873d15e0a2a023f51
indigo-carie <carie.phon@indigoslate.com>
2015-12-30 12:38:35 -0800

    added form

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
-//http://webdesign.tutsplus.com/articles/build-a-multi-step-form-interface--webdesign-11715
+function CreateRecordType() {
+  var obj = $('#survey').serialize();
+  $.ajax({
+      type: "POST",
+      data: JSON.stringify(obj),
+      url: "http://indigostuff.azurewebsites.net/api/recordtypes",
+      contentType: "application/json",
+      success: function () { alert("success"); },
+      error: function(jqXHR, textStatus, errorThrown) { alert(textStatus + ": " + errorThrown); }
+  });
 
-$("#survey").validate({
-  success : function(label){
-    label.addClass("valid").text("✓");
-  },
-  error : function(e){
-//     console.log(e);
-  },
-  onsubmit:false
-});
+  return false; 
+
+};
+
+
+//http://webdesign.tutsplus.com/articles/build-a-multi-step-form-interface--webdesign-11715
 
 $("body").on("keyup", "form", function(e){
   if (e.which == 13){
@@ -20,16 +26,30 @@ $("body").on("keyup", "form", function(e){
   }
 });
 
+var numQuestions = 5; 
+
+$("#back").on("click", function(e){
+  if ($("fieldset.current").index() != 1){
+    e.preventDefault();
+    prevSection();
+    return false;
+  } else {
+    $("#back").addClass("disabled");
+  }
+});
 
 $("#next").on("click", function(e){
-  if ($("#next").is(":visible") && $("fieldset.current").find("input, textarea").valid() ){
+  //if ($("#next").is(":visible") && $("fieldset.current").find("input, textarea").valid() ){
+    if ($("fieldset.current").index() != numQuestions){
       e.preventDefault();
       nextSection();
       return false;
-    }
+    } 
 });
 
-var numQuestions = 6; 
+$("input[type=radio]").on("click", function(e){
+  nextSection();
+});
 
 $("form").on("submit", function(e){
   if ($("#next").is(":visible") || $("fieldset.current").index() < numQuestions || !$("fieldset.current").find("input, textarea").valid()){
@@ -38,36 +58,32 @@ $("form").on("submit", function(e){
   }
 });
   
-  
-function nextSection(){
+function prevSection(){
   var i = $("fieldset.current").index();
-  if (i < numQuestions){
-    $("li").eq(i+1).addClass("active");
-    goToSection(i+1);
+  if (i != 1){
+    goToSection(i-2);
   }
 }
 
-$("li").on("click", function(e){
-  var i = $(this).index();
-  if ($(this).hasClass("active")){
-    goToSection(i);
-  } else {
-    alert("Please complete previous sections first.");
+function nextSection(){
+  var i = $("fieldset.current").index();
+  if (i < numQuestions){
+    goToSection(i++);
   }
-})
-
+}
 
 function goToSection(i){
+  $(".questionNumber span").html(i + 1);
   $("fieldset:gt("+i+")").removeClass("current").addClass("next");
   $("fieldset:lt("+i+")").removeClass("current");
-  $("li").eq(i).addClass("current").siblings().removeClass("current");
+
   setTimeout(function(){
     $("fieldset").eq(i).removeClass("next").addClass("current active");
       if ($("fieldset.current").index() == numQuestions){
-        $("#next").hide();
+        $("#next").html('SKIP & FINISH');
         $("input[type=submit]").show();
       } else {
-        $("#next").show();
+        $("#next").html('SKIP');
         $("input[type=submit]").hide();
       }
   }, 80);
